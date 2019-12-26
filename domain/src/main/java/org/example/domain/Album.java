885083e60fb22bae148d0bb66e8a424d680b2ed2
Bartlomiej Dec <decbartek@gmail.com>
2019-12-26 20:52:33 +0100

    created entities

---
+++ domain/src/main/java/org/example/domain/Album.java
@@ -0,0 +1,31 @@
+package org.example.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.*;
+
+@Entity
+@Table(name = "album")
+@Getter
+@Setter
+public class Album implements Serializable {
+
+    private static final long serialVersionUID = 2003091824768618971L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "album_id")
+    private Long id;
+
+    @Column(name = "trip_date")
+    private Date tripDate;
+
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photos = new ArrayList<>();
+
+    public Album() {
+    }
+}
---
+++ domain/src/main/java/org/example/domain/Photo.java
@@ -0,0 +1,31 @@
+package org.example.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "photo")
+@Getter
+@Setter
+public class Photo implements Serializable {
+
+    private static final long serialVersionUID = 9089647886114593402L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id")
+    private Long id;
+
+    @Lob
+    private Byte[] image;
+
+    @ManyToOne
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    public Photo() {
+    }
+}
