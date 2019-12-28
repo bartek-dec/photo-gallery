da25558214f6d92809b9a87daae50115c1ae3173
Bartlomiej Dec <decbartek@gmail.com>
2019-12-28 19:36:15 +0100

    removed unnecessary comment

--- domain/src/main/java/org/example/domain/Album.java
+++ domain/src/main/java/org/example/domain/Album.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "album")
