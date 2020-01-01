e42ab13fbf1e514ef9e874f675cedc954285060c
Bartlomiej Dec <decbartek@gmail.com>
2020-01-01 20:20:16 +0100

    modified methods inside AlbumController

--- web/src/main/java/org/example/controller/AlbumController.java
+++ web/src/main/java/org/example/controller/AlbumController.java
@@ -1,10 +1,13 @@
 package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.domain.Album;
 import org.example.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,24 +22,32 @@ public class AlbumController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
         log.debug("I am in the AlbumController home()");
+
+        model.addAttribute("albums", albumService.findAllAlbums());
+
+        log.debug("I am in the AlbumController home() after getting data from service");
 
         return "index";
     }
 
     @GetMapping("add_album")
-    public String addAlbum() {
+    public String addAlbum(Model model) {
         log.debug("I am in the AlbumController addAlbum()");
+
+        model.addAttribute("album", new Album());
 
         return "add_album";
     }
 
-    @PostMapping("add_photo")
-    public String createAlbum() {
+    @PostMapping("add_album")
+    public String createAlbum(Album album) {
         log.debug("I am in the AlbumController createAlbum()");
 
-        return "add_photo";
+        albumService.saveAlbum(album);
+
+        return "redirect:/add_photo";
     }
 
     @GetMapping("remove_album")
@@ -51,5 +62,14 @@ public class AlbumController {
         log.debug("I am in the AlbumController editAlbum()");
 
         return "edit_album";
+    }
+
+    @GetMapping("show_album/{id}")
+    public String showAlbum(@PathVariable String id, Model model) {
+        log.debug("I am in the AlbumController showAlbum()");
+
+        model.addAttribute("photos", albumService.findAlbumById(Long.valueOf(id)).getPhotos());
+
+        return "show_album";
     }
 }
