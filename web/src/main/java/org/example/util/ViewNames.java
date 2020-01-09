dac91fc41e777daa8ced827fbf02249f7528d929
Bartlomiej Dec <decbartek@gmail.com>
2020-01-09 22:40:43 +0100

    added class to store view names

---
+++ web/src/main/java/org/example/util/ViewNames.java
@@ -0,0 +1,21 @@
+package org.example.util;
+
+public final class ViewNames {
+
+    public static final String INDEX = "index";
+    public static final String ADD_ALBUM = "add_album";
+    public static final String REMOVE_ALBUM = "remove_album";
+    public static final String EDIT_ALBUM = "edit_album";
+    public static final String SHOW_ALBUM = "show_album";
+    public static final String REDIRECT_REMOVE_ALBUM = "redirect:/remove_album";
+    public static final String REDIRECT_EDIT_ALBUM = "redirect:/edit_album/";
+
+    public static final String ADD_PHOTO = "add_photo";
+    public static final String REDIRECT_ADD_PHOTO = "redirect:/add_photo/";
+    public static final String SHOW = "/show/";
+    public static final String SHOW_IMG = "show_img";
+
+    private ViewNames() {
+
+    }
+}
