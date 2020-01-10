5436473c94415e9685c60dab57a94e28f374fa5d
Bartlomiej Dec <decbartek@gmail.com>
2020-01-10 13:03:46 +0100

    removed unnecessary view names

--- web/src/main/java/org/example/util/ViewNames.java
+++ web/src/main/java/org/example/util/ViewNames.java
@@ -7,12 +7,8 @@ public final class ViewNames {
     public static final String REMOVE_ALBUM = "remove_album";
     public static final String EDIT_ALBUM = "edit_album";
     public static final String SHOW_ALBUM = "show_album";
-    public static final String REDIRECT_REMOVE_ALBUM = "redirect:/remove_album";
-    public static final String REDIRECT_EDIT_ALBUM = "redirect:/edit_album/";
 
     public static final String ADD_PHOTO = "add_photo";
-    public static final String REDIRECT_ADD_PHOTO = "redirect:/add_photo/";
-    public static final String SHOW = "/show/";
     public static final String SHOW_IMG = "show_img";
 
     private ViewNames() {
