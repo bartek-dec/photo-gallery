c5c18b48d9479da2fb22016cb5f024808710b6b9
Bartlomiej Dec <decbartek@gmail.com>
2020-01-10 13:15:49 +0100

    added class to store attribute names in constants

---
+++ web/src/main/java/org/example/util/AttributeNames.java
@@ -0,0 +1,14 @@
+package org.example.util;
+
+public final class AttributeNames {
+
+    public static final String ALBUM = "album";
+    public static final String ALBUMS = "albums";
+    public static final String PHOTOS = "photos";
+    public static final String PHOTO = "photo";
+    public static final String ALBUM_ID = "albumId";
+
+    private AttributeNames() {
+
+    }
+}
