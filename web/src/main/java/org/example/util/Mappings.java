850b6857e7703f4df39772a6cc7e7693aea94eba
Bartlomiej Dec <decbartek@gmail.com>
2020-01-17 16:43:05 +0100

    added two mappings

--- web/src/main/java/org/example/util/Mappings.java
+++ web/src/main/java/org/example/util/Mappings.java
@@ -11,6 +11,10 @@ public final class Mappings {
     public static final String UPDATE_DATA = "updateData";
     public static final String UPDATE_PHOTO = "updatePhoto";
     public static final String SHOW_IMG = "show_img/";
+    
+    //*** Mappings used flags.th.xml ***
+    public static final String LANG_PL = "/?lang=pl";
+    public static final String LANG_EN = "/?lang=en";
 
     public static final String ADD_PHOTO = "add_photo/";
     public static final String PHOTO_ID = "{photoId}";
