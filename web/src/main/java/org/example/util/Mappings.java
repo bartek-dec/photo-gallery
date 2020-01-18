e3e145c85e685e593c30cbfc9dd72581f874ec3d
Bartlomiej Dec <decbartek@gmail.com>
2020-01-18 22:29:32 +0100

    minor changes to Mappings class

--- web/src/main/java/org/example/util/Mappings.java
+++ web/src/main/java/org/example/util/Mappings.java
@@ -8,8 +8,8 @@ public final class Mappings {
     public static final String DELETE = "delete";
     public static final String DISPLAY = "display";
     public static final String DISPLAY_PHOTOS = "displayPhotos";
-    public static final String UPDATE_DATA = "updateData";
-    public static final String UPDATE_PHOTO = "updatePhoto";
+    public static final String UPDATE_DATA = "/updateData";
+    public static final String UPDATE_PHOTO = "/updatePhoto";
     public static final String SHOW_IMG = "/show_img/";
 
     //*** Mappings used flags.th.xml ***
@@ -32,8 +32,8 @@ public final class Mappings {
     public static final String DISPLAY_PHOTOS_ALBUM_ID_PHOTO_ID_DISPLAY =
             DISPLAY_PHOTOS + SLASH + ALBUM_ID + SLASH + PHOTO_ID + SLASH + DISPLAY;
     public static final String REMOVE_ALBUM_ALBUM_ID_DELETE = REMOVE_ALBUM + ALBUM_ID + SLASH + DELETE;
-    public static final String EDIT_ALBUM_ALBUM_ID_UPDATE_DATA = EDIT_ALBUM + ALBUM_ID + SLASH + UPDATE_DATA;
-    public static final String EDIT_ALBUM_ALBUM_ID_UPDATE_PHOTO = EDIT_ALBUM + ALBUM_ID + SLASH + UPDATE_PHOTO;
+    public static final String EDIT_ALBUM_ALBUM_ID_UPDATE_DATA = EDIT_ALBUM + ALBUM_ID + UPDATE_DATA;
+    public static final String EDIT_ALBUM_ALBUM_ID_UPDATE_PHOTO = EDIT_ALBUM + ALBUM_ID + UPDATE_PHOTO;
     public static final String EDIT_ALBUM_ALBUM_ID_PHOTO_ID_DELETE =
             EDIT_ALBUM + ALBUM_ID + SLASH + PHOTO_ID + SLASH + DELETE;
     public static final String ADD_PHOTO_ALBUM_ID_SHOW_PHOTO_ID_DELETE =
