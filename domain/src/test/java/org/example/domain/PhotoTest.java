05da873c67156281bb1c9636861006042e4baed0
Bartlomiej Dec <decbartek@gmail.com>
2020-01-29 10:24:30 +0100

    unit tests modified in PhotoTest

--- domain/src/test/java/org/example/domain/PhotoTest.java
+++ domain/src/test/java/org/example/domain/PhotoTest.java
@@ -7,19 +7,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PhotoTest {
 
+    private Photo photo;
+    private Photo blankPhoto;
+    private final Long id = 1L;
+    private final byte[] image = new byte[1];
+    private final Album album = new Album();
+
     @BeforeEach
     void setUp() {
+        photo = new Photo();
+        blankPhoto = new Photo();
+
+        photo.setId(id);
+        photo.setImage(image);
+        photo.setAlbum(album);
     }
 
     @Test
     void getId() {
+        Long photoId = 1L;
+
+        assertEquals(photoId, photo.getId());
+    }
+
+    @Test
+    void nullWhenNoId() {
+        assertNull(blankPhoto.getId());
     }
 
     @Test
     void getImage() {
+        byte[] array = new byte[1];
+
+        assertArrayEquals(array, photo.getImage());
+    }
+
+    @Test
+    void nullWhenNoImage() {
+        assertNull(blankPhoto.getImage());
     }
 
     @Test
     void getAlbum() {
+        Album photosAlbum = new Album();
+
+        assertEquals(photosAlbum, photo.getAlbum());
+    }
+
+    @Test
+    void nullWhenNoAlbum() {
+        assertNull(blankPhoto.getAlbum());
     }
 }
